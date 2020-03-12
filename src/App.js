364585f1a7715ee218082ab640abf7a556d7120d
Jason Guarracino <gooeydev@gmail.com>
2020-03-12 07:30:36 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="em">
+            <div className="em-header">
+                <Navbar bg="dark" expand="lg" sticky="top">
+                    <Navbar.Brand href="#home"><span class="em-logo">em</span></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="/journal">Journal</Nav.Link>
+                        <Nav.Link href="/comfort">Comfort</Nav.Link>
+                        <Nav.Link href="/reflect">Reflect</Nav.Link>
+                        <Nav.Link href="/settings">Settings</Nav.Link>
+                        <Nav.Link href="/help">Help</Nav.Link>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+            <Container>
+                <Row>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
